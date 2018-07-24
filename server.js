2411071c0d0a284c32fc47233d77e4a6383cee3b
Shineka Barnett <shinekabarnett@github.com>
2018-07-24 08:38:22 -0400

    heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use("/", serveStatic (path.join (__dirname, 'public')))
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+const port = process.env.PORT || 5000
+app.listen(port)
+
+console.log('Server stated on port ' + port)
